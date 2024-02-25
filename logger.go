297845c37746def8d3e878911de657ce7cea4aec
hossam <hossamsulleman@gmail.com>
2024-02-25 15:35:49 +0000

    Revert "push"
    This reverts commit 418e767674245979ffe8a998b3708427059c8415.

--- logger.go
+++ logger.go
@@ -2,40 +2,38 @@ package logger
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/fatih/color"
-	"github.com/sanity-io/slog"
 )
-
-var decoration = `%c $: -------------------------------------------------------------------`
 
 // Info logs informational messages with formatted string.
 func Info(format string, args ...interface{}) {
-	logger := slog.New(os.Stdout, slog.ColorDecoratedOutput)
-	logger.Info().Msg(fmt.Sprintf(decoration+"\nINFO: "+format, append([]interface{}{color.GreenString()}, args...)...))
+	infoLog := log.New(os.Stdout, color.GreenString("INFO: "), log.Ldate|log.Ltime)
+	infoLog.Println(fmt.Sprintf(format, args...))
 }
 
 // Warning logs warning messages with formatted string.
 func Warning(format string, args ...interface{}) {
-	logger := slog.New(os.Stdout, slog.ColorDecoratedOutput)
-	logger.Warn().Msg(fmt.Sprintf(decoration+"\nWARNING: "+format, append([]interface{}{color.YellowString()}, args...)...))
+	warningLog := log.New(os.Stdout, color.YellowString("WARNING: "), log.Ldate|log.Ltime|log.Lshortfile)
+	warningLog.Println(fmt.Sprintf(format, args...))
 }
 
 // Error logs error messages with formatted string.
 func Error(format string, args ...interface{}) {
-	logger := slog.New(os.Stderr, slog.ColorDecoratedOutput)
-	logger.Error().Msg(fmt.Sprintf(decoration+"\nERROR: "+format, append([]interface{}{color.RedString()}, args...)...))
+	errorLog := log.New(os.Stderr, color.RedString("ERROR: "), log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog.Println(fmt.Sprintf(format, args...))
 }
 
 // Success logs success messages with formatted string.
 func Success(format string, args ...interface{}) {
-	logger := slog.New(os.Stdout, slog.ColorDecoratedOutput)
-	logger.Info().Msg(fmt.Sprintf(decoration+"\nSUCCESS: "+format, append([]interface{}{color.MagentaString()}, args...)...))
+	successLog := log.New(os.Stdout, color.MagentaString("SUCCESS: "), log.Ldate|log.Ltime)
+	successLog.Println(fmt.Sprintf(format, args...))
 }
 
 // Debug logs debug messages with formatted string.
 func Debug(format string, args ...interface{}) {
-	logger := slog.New(os.Stdout, slog.ColorDecoratedOutput)
-	logger.Debug().Msg(fmt.Sprintf(decoration+"\nDEBUG: "+format, append([]interface{}{color.BlueString()}, args...)...))
+	debugLog := log.New(os.Stdout, color.BlueString("DEBUG: "), log.Ldate|log.Ltime)
+	debugLog.Println(fmt.Sprintf(format, args...))
 }
